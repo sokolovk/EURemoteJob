0de0b5b1b9a609e395e96c29aae8e1f4f5ef6ed4
kostiantyn <kostiantynf5@gmail.com>
2022-07-01 19:59:40 +0300

    edit hero

--- wp-content/themes/EU-remote-jobs/src/main.js
+++ wp-content/themes/EU-remote-jobs/src/main.js
@@ -1,5 +1,7 @@
 // import styles
+alert('main.js');
 import '@scss/main.scss';
 import '@lib/scss/base.lib.scss';
 // require main js file
 require('@js/index');
+
--- wp-content/themes/EU-remote-jobs/template-parts/sections/home-hero.php
+++ wp-content/themes/EU-remote-jobs/template-parts/sections/home-hero.php
@@ -8,6 +8,7 @@ $padding_bottom_tablet = $hero_params['padding_bottom_tablet'] ? $hero_params['p
 $padding_bottom_mobile = $hero_params['padding_bottom_mobile'] ? $hero_params['padding_bottom_mobile'] : 50;
 $showImgBg = $hero_params['show_background_image'];
 $urlImgBg = $hero_params['background_image'];
+$hero_btn = get_field('hero_button');
 
 $title_color = get_field('title_color');
 $title = get_field('hero_title');
@@ -25,21 +26,45 @@ $GLOBALS['custom-styles']
 ?>
 
 <section class="hero lozad"
-         style="<?php if($showImgBg && $urlImgBg) { ?> background-image: url('<?php echo esc_url($urlImgBg);?>'); <?php } ?>
-             background-color: <?php echo $hero_params['background_color']; ?>;
+         style="background-color: <?php echo $hero_params['background_color']; ?>;
              border-bottom-left-radius: <?php echo $hero_params['bottom_left_radius']; ?>px;
              border-bottom-right-radius: <?php echo $hero_params['bottom_right_radius']; ?>px;
              color: <?php echo $hero_params['text_color']; ?>">
 
     <div class="page-container">
 
-        <?php if($title) { ?>
-            <div class="hero__title" style="color: <?php echo $title_color?>;"><?php echo $title; ?></div>
-        <?php } ?>
+        <div class="hero__wrap">
 
-        <?php if($title) { ?>
-            <div class="hero__content"><?php echo $content; ?></div>
-        <?php } ?>
+            <div class="hero__col-content">
+                <?php if($title) { ?>
+                    <div class="hero__title" style="color: <?php echo $title_color?>;"><?php echo $title; ?></div>
+                <?php } ?>
+
+                <?php if($title) { ?>
+                    <div class="hero__content"><?php echo $content; ?></div>
+                <?php } ?>
+
+                <?php if($hero_btn['show_button'] && $hero_btn['text']) { ?>
+                    <div class="hero__btn-wrap position-relative">
+                        <button class="btn position-relative">
+                            <?php echo $hero_btn['text']; ?>
+                        </button>
+                        <div class="outline"></div>
+                    </div>
+                <?php } ?>
+            </div>
+
+            <div class="hero__col-image">
+                <?php if($showImgBg && $urlImgBg) { ?>
+                    <figure class="hero__image">
+                        <img class="lozad" data-src="<?php echo esc_url($urlImgBg);?>" alt="" />
+                    </figure>
+                <?php } ?>
+            </div>
+
+        </div>
+
+
 
     </div>
 
